511e863b341a479dad3b389c5667fe5eb78eb7e9
pclank <panosclank@gmail.com>
2020-11-19 23:55:04 +0200

    Added Function for Generating Non-Repeating Indices for the Initialization of Centroids.
    Signed-off-by: pclank <panosclank@gmail.com>

--- kmeans.c
+++ kmeans.c
@@ -4,19 +4,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Definitions - Macros
-#define Max 100         // Upper Absolute Value Limit of Vector Values
+#define Max 10         // Upper Absolute Value Limit of Vector Values
 #define N 10           // Number of Vectors
-#define Nv 10           // Number of Dimensions
-#define Nc 5            // Number of Clusters
+#define Nv 3           // Number of Dimensions
+#define Nc 3            // Number of Centroids
 
 // Declare Arrays
+int index_array[Nc];
 float vectors[N][Nv];
+float centroids[Nc][Nv];
+
+//**********************************************************
+// Fix to Generate Random Sequence of Non-Repeating Integers
+//**********************************************************
+
+void generateIndices(void)
+{
+    srand(time(NULL));          // Create rand() Seed
+
+    int temp;
+    unsigned int flag = 0;                  // Duplicate Flag
+
+    int i = 0;
+    while (i != Nc)
+    {
+        temp = rand() % Nc;
+//        printf("%d\n", temp);
+
+        for (int j = 0; j < i; j++)
+        {
+            if (temp == index_array[j])             // Check if Duplicate
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (!flag)                              // If NOT Duplicate, Add to Array
+        {
+            index_array[i] = temp;
+
+            i++;
+        }
+
+        flag = 0;
+    }
+}
+
 
 // Function Creating N Random Vectors of Size Nv
 void createVectors(void)
 {
+    srand(time(NULL));          // Create rand() Seed
+
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < Nv; j++)
@@ -26,6 +69,12 @@ void createVectors(void)
         }
         puts("\n");
     }
+}
+
+// Function to Initialize Centroids
+void initCentroids(void)
+{
+
 }
 
 // Driver Function
